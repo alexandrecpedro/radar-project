0cdd4bba4f254424753019ba205f2d130f1958cb
Gabriel Gualberto <gabrielgualbertooliveira@gmail.com>
2022-12-01 16:51:39 -0300

    feat: ClientService e Product Interface

--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product.interface';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
 
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.productService = new ProductService(this.http);
+    this.getProducts();
+  }
+
+  private productService: ProductService = {} as ProductService;
+  public products: Product[] | undefined = [];
+  public product: Product = {} as Product;
+
+  private async getProducts(){
+    this.products = await this.productService.getProduct();
+  }
+
+  create(){
+    this.productService.createProduct({
+      id: 0,
+      name: this.product.name,
+      description: this.product.description,
+      stockQty: this.product.stockQty,
+      value: this.product.value
+    });
+    this.getProducts();
+  }
+  
+  async delete(product: Number){
+    await this.productService.deleteProduct(product)
+    this.products = await this.productService.getProduct();
+  }
+
+  selectProduct(producte: Product){
+    this.product = producte;
+  }
+
+  async save(){
+    if(this.product.id && this.product.id != 0){
+        const update = await this.productService.updateProduct(this.product);
+        console.log(update);
+    }
+  }
 }
--- frontend/src/app/services/product/product.service.ts
+++ frontend/src/app/services/product/product.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { Product } from 'src/app/interfaces/product.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public async getProduct(): Promise<Product[] | undefined>{
+    let products:Product[] | undefined = await firstValueFrom(this.http.get<Product[]>(`${environment.api}/products`));
+    return products;
+  }
+
+  public async createProduct(product: Product): Promise<Product | undefined>{
+    let newProduct:Product | undefined = await firstValueFrom(this.http.post<Product>(`${environment.api}/products`, product));
+    return newProduct; 
+  }
+
+  public async deleteProduct(productId: Number){
+    await firstValueFrom(this.http.delete(`${environment.api}/Products/${productId}`));
+  }
+
+  public async updateProduct(product: Product): Promise<Product | undefined>{
+    let ProductUpdate: Product | undefined = await firstValueFrom(this.http.put<Product>(`${environment.api}/Products/${product.id}`, product ));
+    return ProductUpdate;
+
+  }
 }
