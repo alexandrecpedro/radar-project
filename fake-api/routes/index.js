fa3fe7c877c6a65d1ae4a255cf257ae212a84bcc
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-12-01 20:56:58 -0300

    feat: orders

--- fake-api/routes/index.js
+++ fake-api/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const HomeController = require('../controllers/homeController');
 const ClientsController = require('../controllers/clientsController');
 const ProductsController = require('../controllers/productsController');
+const OrdersController = require('../controllers/ordersController');
 
 /* GET home page. */
 router.get('/', HomeController.index);
@@ -20,5 +21,12 @@ router.post('/products', ProductsController.create);
 router.get('/products/:id', ProductsController.show);
 router.delete('/products/:id', ProductsController.delete);
 router.put('/products/:id', ProductsController.update);
+
+/* Orders */
+router.get('/orders', OrdersController.index);
+router.post('/orders', OrdersController.create);
+router.get('/orders/:id', OrdersController.show);
+router.delete('/orders/:id', OrdersController.delete);
+router.put('/orders/:id', OrdersController.update);
 
 module.exports = router;
