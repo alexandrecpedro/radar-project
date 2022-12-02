e3051acb30a4d90bf118608590bc6352845e0e85
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-12-02 20:15:32 -0300

    refactor: routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth/auth.guard';
+import { EditPermissionGuard } from './guards/edit-permission/edit-permission.guard';
 import { CashFlowComponent } from './pages/cash-flow/cash-flow.component';
 import { ClientsComponent } from './pages/clients/clients.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -12,8 +13,32 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'clients', component: ClientsComponent, canActivate: [AuthGuard] },
+  {
+    path: 'clients/:id', 
+    canActivateChild: [EditPermissionGuard],
+    children: [
+      { path: '', redirectTo: 'update', pathMatch: 'full' },
+      { path: 'update', component: ClientsComponent, canDeactivate: [FormLeaveGuard] }
+    ]
+  },
   { path: 'orders', component: OrdersComponent, canActivate: [AuthGuard] },
+  {
+    path: 'orders/:id', 
+    canActivateChild: [EditPermissionGuard],
+    children: [
+      { path: '', redirectTo: 'update', pathMatch: 'full' },
+      { path: 'update', component: OrdersComponent, canDeactivate: [FormLeaveGuard] }
+    ]
+  },
   { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] },
+  {
+    path: 'products/:id', 
+    canActivateChild: [EditPermissionGuard],
+    children: [
+      { path: '', redirectTo: 'update', pathMatch: 'full' },
+      { path: 'update', component: OrdersComponent, canDeactivate: [FormLeaveGuard] }
+    ]
+  },
   { path: 'cash-flow', component: CashFlowComponent, canActivate: [AuthGuard] }
 ];
 
