9dd9da9b7e5865b56ef329a7681a973fcfdd5d2b
Gabriel Gualberto <gabrielgualbertooliveira@gmail.com>
2022-12-02 21:02:04 -0300

    Feat: Client Observers

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClientObserverService } from 'src/app/services/client/client-observer.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+    constructor(public clientObserver: ClientObserverService){}
 }
--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
   logar(){
     console.log(this.email)
     if(this.email === "radar@login.com" && this.senha === "12345"){
-      localStorage.setItem("logado", "true")
+      localStorage.setItem("logged", "true")
       this.router.navigateByUrl("/home")
       
     }
